305e89a783c0f29eb2932243d2cb0875eee77f1b
Vijay-Hozo <vijay2304a@gmail.com>
2024-12-27 17:47:50 +0530

    dashboard setup completed

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,65 @@
+import { Geist } from "next/font/google";
+import {
+    IconHome,
+    IconNewSection,
+    IconTerminal2,
+  } from "@tabler/icons-react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+const geistSans = Geist({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    const links = [
+        {
+          title: "Home",
+          icon: (
+            <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+     
+        {
+          title: "Products",
+          icon: (
+            <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+        {
+          title: "Components",
+          icon: (
+            <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+    ]
+  return (
+    <div className="flex w-full gap-10 justify-evenly ">
+      <div>
+        <FloatingDock desktopClassName="flex flex-col min-h-[200px] justify-center translate-y-20"
+        items={links}
+      />
+      </div>
+      <div>
+        {children}
+      </div>
+    </div>
+  );
+}
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,11 @@
-
+//"Dream big, plan small, achieve more."
 export default function Header() {
   return (
-    <div>
-      <h1>Welcome to ZenTask</h1>
-    </div>
+    <section className="min-h-screen w-full flex-col">
+      <div className=" text-center">
+        <h1 className="text-5xl mb-5 font-bold">Dream big Plan Small</h1>
+        <h1 className="text-6xl font-semibold">Achieve More</h1>
+      </div>
+    </section>
   );
 }
